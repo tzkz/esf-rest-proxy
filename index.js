fe2117304094b05dd341979895addc76a835dd8b
Timur Z <engineer@timurz.com>
2019-03-03 14:55:40 +0600

    move createService() to a separate file

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const soap = require('soap')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const { Jsonix } = require('jsonix')
 
-const config = require('./config')
+const createService = require('./soapService')
 const mappingInvoiceV2 = require('./mappings/invoiceV2').PO
 
 const context = new Jsonix.Context([mappingInvoiceV2])
@@ -117,32 +116,6 @@ const startApp = ([invoiceService, sessionService]) => {
     console.log('Listening on port:', port) // eslint-disable-line no-console
   })
 }
-
-const createService = (name, options = {}) => new Promise((resolveService, rejectService) => {
-  soap.createClient(config.wsdl[name], options, (errorService, client) => {
-    if (errorService) {
-      rejectService(errorService)
-    }
-    console.log(`${name} SOAP client has been created`) // eslint-disable-line no-console
-    resolveService((method, requestOptions) => new Promise((resolve, reject) => {
-      const { username, password, body } = requestOptions
-
-      if (name === 'session') {
-        client.setSecurity(new soap.WSSecurity(username, password, {
-          hasTimeStamp: false,
-          hasTokenCreated: false,
-        }))
-      }
-
-      client[method](body, (error, result) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(result)
-      }, { rejectUnauthorized: false })
-    }))
-  })
-})
 
 Promise.all([
   createService('invoice'),
